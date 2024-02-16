6ccd71a9df9b76e31d2d6f18257e34d0f0f9d5dc
DonaldTaggart <105013892+DonaldTaggart@users.noreply.github.com>
2024-02-15 16:40:20 -0800

    Update Graph.svelte
    Adding the x-axis label: "Year"

--- src/components/Graph.svelte
+++ src/components/Graph.svelte
@@ -162,12 +162,23 @@
         >
 
         <!-- x-axis -->
-        <g bind:this={gx} transform="translate(0,{height - marginBottom})" />
+        <g bind:this={gx} transform="translate(0,{height - marginBottom})" >
+            <text
+                x="5"
+                y={marginTop}
+                dy="0.32em"
+                fill="#000"
+                font-weight="bold"
+                text-anchor="start"
+            >
+                Year
+            </text>
+        </g>
         <!-- y-axis -->
         <g bind:this={gy} transform="translate({marginLeft},0)">
             <text
                 x="5"
-                y={marginTop}
+                y={marginBottom}
                 dy="0.32em"
                 fill="#000"
                 font-weight="bold"
